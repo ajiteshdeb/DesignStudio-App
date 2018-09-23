c2496678c485faccc711f878ad2556b65f75b5d6
Ajitesh Deb <ajitesh.deb@gmail.com>
2018-09-24 01:34:29 +0530

    domain changed again to design-studio.io in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
   production: false,
-  origin: 'http://design-studio.io/ETthemes/wp-json'
+  origin: 'http://design-studio.io/wp-json'
 };
 
 /*
